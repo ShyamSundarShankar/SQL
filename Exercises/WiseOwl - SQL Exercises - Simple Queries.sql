1855b66f77c706b87b9845b24e3a30b3f5de1f49
Shyam Sundar Shankar <Shyam.dragon89@gmail.com>
2018-11-07 02:59:40 +0530

    Completed Simple Queries Exercise
    Completed Simple Queries Exercise by WiseOwl

--- Exercises/WiseOwl - SQL Exercises - Simple Queries.sql
+++ Exercises/WiseOwl - SQL Exercises - Simple Queries.sql
@@ -1,4 +1,12 @@
+--List out the events in world history in date order--
+Select EventName, EventDate from WorldEvents.dbo.tblEvent ORDER BY EventDate DESC;
+
+--Show the first 5 rows from events table, with column aliases--
+Select TOP (5) EventName AS What, EventDetails AS Details from WorldEvents.dbo.tblEvent ORDER BY EventDate ASC;
+
 --Show the Last 3 categories in alphabetical order--
 Select TOP (3) CategoryID, CategoryName from WorldEvents.dbo.tblCategory ORDER BY CategoryName DESC;
 
---
+--Show the first and the last two events in date order--
+Select TOP (2) EventName AS What, EventDate AS [When] from WorldEvents.dbo.tblEvent ORDER BY EventDate ASC
+Select TOP (2) EventName AS What, EventDate AS [When] from WorldEvents.dbo.tblEvent ORDER BY EventDate DESC;
