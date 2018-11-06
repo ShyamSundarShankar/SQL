22819867bc58acda23c74530adbfa6b2b66abf21
Shyam Sundar Shankar <Shyam.dragon89@gmail.com>
2018-11-07 03:20:03 +0530

    Pended Exercise Pushed
    Pushed Exercise which has been done so far

---
+++ Exercises/WiseOwl - SQL Exercises - Setting Criteria using Where.sql
@@ -0,0 +1,4 @@
+--List out events to do with Teletubbies or Andy Pandy--
+Select EventName, EventDate from WorldEvents.dbo.tblEvent WHERE EventName LIKE '%Teletubbies%' OR EventName LIKE '%Pandy%';
+
+--Combine criteria to show possibly watery post-1970 events!--
