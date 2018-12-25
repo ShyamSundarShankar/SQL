c848397ce082285c8506e5b7e57ab12c09aa4806
Shyam Sundar Shankar <Shyam.dragon89@gmail.com>
2018-12-25 20:41:10 +0530

    Delete WiseOwl - Calculations.sql

--- Exercises/WiseOwl - Calculations.sql
+++
@@ -1,7 +0,0 @@
---Use the LEN function to show events with the shortest names--
-Select EventName, LEN(EventName) as 'Length of name' from WorldEvents.dbo.tblEvent ORDER BY [Length of name] ASC;
-
---Show how big countries are in relation to Wales--
-Select Country, KmSquared, ((KmSquared - (KmSquared % 20761))/20761) as 'WalesUnits', (KmSquared % 20761) as 'AreaLeftOver', CAST(CASE WHEN ((KmSquared - (KmSquared % 20761))/20761) = 0 THEN 'Smaller than Wales' ELSE (((KmSquared - (KmSquared % 20761))/20761) + 'x Wales plus ' + (KmSquared % 20761) + ' sq. kms.') END AS 'WalesComparison') from WorldEvents.dbo.CountriesByArea order by Country ASC;
-
---Concatenate text to show events with their categories--
